2824de925653e60f9ac7145e1f75b872c7af01c9
Abhi <abhi.desikan@gmail.com>
2014-11-09 22:51:11 -0500

    updatedpython

--- Scripts/twitter.py
+++ Scripts/twitter.py
@@ -1,7 +1,11 @@
 import powerlaw
 import sys,ast
 
-data = ast.literal_eval( sys.argv[1] )
+filename = sys.argv[1]
+
+f = open(filename,'r')
+
+data = ast.literal_eval( f.read() )
 results = powerlaw.Fit(data)
 print "Gamma value", results.power_law.alpha
 	
--- src/com/ub/twittersimulator/utilities/Python.java
+++ src/com/ub/twittersimulator/utilities/Python.java
@@ -4,7 +4,9 @@
 package com.ub.twittersimulator.utilities;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -27,17 +29,37 @@ public class Python {
 	 * as input parameter.
 	 */
 	public void getGammaValue(List<Node> nodeList) {
+		String items = "";
+		try{
+			File file2 = new File(
+					"/home/abhi/workspaceLuna/TwitterSimulator/Arguments");
+			if (!file2.exists()) {
+				file2.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file2.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write("[");
+			for (Node node : nodeList) {			
+				items = items + (node.getFollowers().size() + ",");
+				
+			}
+			bw.write(items);
+			bw.write("]");
+			bw.close();
+			fw.close();
 
-		String items = "[";
-		for (Node node : nodeList) {
-			items = items + (node.getFollowers().size() + ",");
+			
+		}catch(IOException e){
+			e.printStackTrace();
 		}
-		items = items + "]";
+
+	
+
 		String line = null;
 		int gamma = 0;
 
 		try {
-			Process p = Runtime.getRuntime().exec("python "+System.getProperty("user.dir")+"/Scripts/twitter.py "+ items);
+			Process p = Runtime.getRuntime().exec("python "+System.getProperty("user.dir")+"/Scripts/twitter.py /home/abhi/workspaceLuna/TwitterSimulator/Arguments");
 			BufferedReader bri = new BufferedReader(new InputStreamReader(
 					p.getInputStream()));
 			while ((line = bri.readLine()) != null) {
